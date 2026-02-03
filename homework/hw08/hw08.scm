165f1aa28aa341e71fd2287ca69328fb54a34e54
Xushuaiwei Shi <sxsw1029@qq.com>
2026-02-03 12:13:39 +0800

    hw08 finished

--- homework/hw08/hw08.scm
+++ homework/hw08/hw08.scm
@@ -1,7 +1,26 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+  (if (or (null? s) (null? (cdr s)))
+    #t
+    (if (> (car s) (car (cdr s)))
+      #f
+      (ascending? (cdr s)))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+  (if (null? s)
+    s
+    (if (pred (car s))
+      (cons (car s) (my-filter pred (cdr s)))
+      (my-filter pred (cdr s)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+  (cond
+    ((null? lst1) lst2)
+    ((null? lst2) lst1)
+    (else (cons (car lst1) (interleave lst2 (cdr lst1))))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s)
+  (if (null? s)
+    s
+    (cons
+      (car s)
+      (no-repeats (filter (lambda (x) (not (= (car s) x))) (cdr s))))))
