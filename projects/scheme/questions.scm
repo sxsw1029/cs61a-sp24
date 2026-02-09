93efe5edf29e3fd94730723b0d0075c27819082d
Xushuaiwei Shi <sxsw1029@qq.com>
2026-02-10 00:41:53 +0800

    project scheme part 4 finished

--- projects/scheme/questions.scm
+++ projects/scheme/questions.scm
@@ -7,7 +7,12 @@
 ;; Returns a list of two-element lists
 (define (enumerate s)
   ; BEGIN PROBLEM 15
-  'replace-this-line
+    (define (helper lst idx)
+      (if (null? lst)
+        '()
+        (cons (list idx (car lst)) (helper (cdr lst) (+ idx 1)))))
+
+    (helper s 0)
   )
   ; END PROBLEM 15
 
@@ -17,7 +22,14 @@
 ;; the merged lists.
 (define (merge ordered? s1 s2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
+    (cond
+      ((null? s1) s2)
+      ((null? s2) s1)
+
+      ((ordered? (car s1) (car s2))
+        (cons (car s1) (merge ordered? (cdr s1) s2)))
+      (else
+        (cons (car s2) (merge ordered? s1 (cdr s2)))))
   )
   ; END PROBLEM 16
 
